8d8dabddc289f443b17f0bd2c2b63f0a108adbe0
Abu-muaz <plshireseid@gmail.com>
2025-09-24 19:27:25 +0300

    deploying

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Amazon__Frontend",
+  base: "/Amazon__Frontend",
+    build: {
+    chunkSizeWarningLimit: 1000, 
+  },
   plugins: [react()],
   server: {
     port:3000,
   }
 })
+
+
+// 
